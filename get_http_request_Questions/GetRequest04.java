4345309a215a00c53abfa5ef43165c3d6a79d20c
Ilknur Turksever <ilknurturksever1819@gmail.co>
2022-03-28 00:47:39 +0300

    Q

---
+++ get_http_request_Questions/GetRequest04.java
@@ -0,0 +1,44 @@
+package get_http_request_Questions;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+
+public class GetRequest04 {
+
+    /* POAITIVE SCENARIO :
+         When I send a GET Request to
+         https://dummy.restapiexample.com/api/v1/employees
+         And Accept type "application/JSON"
+         Then
+         HTTP  Sattus Code should be 200
+         And Response format should be "apllication/JSON"
+         And there should be 24 employees
+         And "Ashton Cox" should be one of the employees
+         And 21, 61, and 63 should be among the employee ages
+     */
+
+    @Test
+    public void get01(){
+        Response response = given().
+                               accept(ContentType.JSON). //"application/JSON" da kullanlabilir
+                            when().
+                               get("https://dummy.restapiexample.com/api/v1/employees");
+               response.prettyPrint();
+
+               response.
+                       then().
+                       assertThat().
+                       statusCode(200).
+                       contentType(ContentType.JSON).//"application/JSON" da kullanlabilir
+                       body("data.id",Matchers.hasSize(24)).
+                       body("data.employee_name",Matchers.hasItem("Ashton Cox")).
+                       body("data.employee_age",Matchers.hasItems("21,61,63"));
+
+    }
+
+}
---
+++ get_http_request_Questions/GetRequest05.java
@@ -0,0 +1,37 @@
+package get_http_request_Questions;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import static io.restassured.RestAssured.*;
+
+
+public class GetRequest05 {
+    /*
+       When I send a GET request to REST API URL
+       https://restful-booker.herokuapp.com/booking/5
+       Then HTTP Status Code should be 200
+       And response content type is "application/JSON"
+       And "firstname" should be "Jim",
+       And "totalprice" should be 602,
+       And "checkin" should be "2015-06-12"
+     */
+
+    @Test
+    public void get01(){
+        Response response = given().
+                        when().
+                get("https://restful-booker.herokuapp.com/booking/5");
+        response.prettyPrint();
+
+        response.
+                then().
+                assertThat().
+                statusCode(200).
+                contentType(ContentType.JSON).
+                body("firstname", Matchers.equalTo("Jim")).
+                body("totalprice",Matchers.equalTo(509)).
+                body("checkin",Matchers.equalTo("2015-09-28"));
+    }
+}
