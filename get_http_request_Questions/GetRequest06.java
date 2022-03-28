fa1990acb3855edf75dd46d0f0b43079c1973112
ilknurturksever <ilknurturksever1819@gmail.com>
2022-03-29 01:23:03 +0300

    Q

--- get_http_request_Questions/GetRequest06.java
+++ get_http_request_Questions/GetRequest06.java
@@ -1,11 +1,19 @@
 package get_http_request_Questions;
 
+import TestBase.TestBase;
+import io.restassured.response.Response;
 import org.junit.Test;
 
-public class GetRequest06 {
+import static io.restassured.RestAssured.*;
+
+public class GetRequest06 extends TestBase {
+
+    // TestBase class olusturup her testte kullanilan data'lari TestBase class'a koyup
+    // tekrar tekrar ayni seyleri yazmaktan kurtulacagiz
 
     @Test
     public void get01(){
 
+        Response response = given().spec(spec01).when().get();
     }
 }
